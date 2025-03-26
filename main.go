84433d26980e867dd8b5503fb10236172117793a
BVoid <atlas.pywork@gmail.com>
2025-03-25 19:42:17 -0700

    Add files via upload

--- main.go
+++ main.go
@@ -1,4 +1,4 @@
-// Package main provides a simple command-line tool to read and display the contents of a file.
+// Package provides a simple command-line tool to read and display the contents of a file.
 // sofi Alpha
 package main
 
@@ -18,10 +18,38 @@ func main() {
 	if len(os.Args) < 2 { // check if there are enough arguments
 		fmt.Println(colorRed + "Not enough arguments! , Please provide a file name." + colorReset) // add a comment here
 		return
-	} else if len(os.Args) > 2 { // check if there are too many arguments
-		fmt.Println(colorRed + "Too many arguments! , Please provide a file name." + colorReset) // add a comment here
-		return
-	} else {
+	} else if len(os.Args) > 2 && os.Args[1] != "-m" { // check if there are too many arguments
+		for _, arg := range os.Args { // iterate over the arguments
+			file, err := os.Open(arg) // open the file
+			if err != nil {           // handle the error if the file cannot be opened
+				fmt.Println(colorRed + err.Error() + colorReset) // print the error message
+			}
+			defer file.Close()          // close the file when the function returns
+			data, _ := os.ReadFile(arg) // read the file contents
+			fmt.Println(string(data))   // print the file contents
+		}
+	} else if os.Args[1] == "." { // list all files in the current directory
+		wd, _ := os.Getwd()          // get the current working directory
+		files, _ := os.ReadDir(wd)   // read the directory contents
+		for _, file := range files { // iterate over the files in the directory
+			file, err := os.Open(file.Name()) // open the file
+			if err != nil {                   // handle the error if the file cannot be opened
+				fmt.Println(colorRed + err.Error() + colorReset) // print the error message
+			}
+			defer file.Close()                                                                                                                            // close the file when the function returns
+			data, _ := os.ReadFile(file.Name())                                                                                                           // read the file contents
+			fmt.Println(colorYellow, file.Name(), colorReset, colorGreen, "\nstart->(\n", colorReset, string(data), colorGreen, "\n)\n<-end", colorReset) // print the file contents
+		}
+	} else if os.Args[1] == "-m" { // working this is a bug (multi args reading file)
+		for _, arg := range os.Args[2:] { // iterate over the arguments
+			file, err := os.ReadFile(arg)
+			if err != nil {
+				fmt.Println(colorRed + err.Error() + colorReset)
+			}
+			fmt.Println(colorYellow, arg, colorReset, colorGreen, "\nstart->(\n", colorReset, string(file), colorGreen, "\n)<-end", colorReset) // print the file contents
+		}
+
+	} else if len(os.Args) == 2 && os.Args[1] != "-m" {
 		op := os.Args[1]         // get the file name from the command line arguments
 		file, err := os.Open(op) // open the file
 		if err != nil {          // handle the error if the file cannot be opened
@@ -30,6 +58,5 @@ func main() {
 		defer file.Close()         // close the file when the function returns
 		data, _ := os.ReadFile(op) // read the file contents
 		fmt.Println(string(data))  // print the file contents
-
 	}
 }
